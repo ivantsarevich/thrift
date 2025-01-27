4ba55243074378a3d74168e281db60bd45690cbd
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-27 19:58:06 +0300

    Initialize conditional bank

---
+++ thrift-bank/src/test/java/efr/iv/igr/thriftbank/ThriftBankApplicationTests.java
@@ -0,0 +1,13 @@
+package efr.iv.igr.thriftbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThriftBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
