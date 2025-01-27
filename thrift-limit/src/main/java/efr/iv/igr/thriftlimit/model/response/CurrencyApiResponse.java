59f46c0d4d7397c08025d06aaff2ccb28b69fb4d
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-28 02:18:18 +0300

    Update currency api response model and add transaction response model

--- thrift-limit/src/main/java/efr/iv/igr/thriftlimit/model/response/CurrencyApiResponse.java
+++ thrift-limit/src/main/java/efr/iv/igr/thriftlimit/model/response/CurrencyApiResponse.java
@@ -1,6 +1,7 @@
 package efr.iv.igr.thriftlimit.model.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import efr.iv.igr.thriftlimit.model.entity.CurrencyCode;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -10,7 +11,7 @@ import java.util.Map;
 public class CurrencyApiResponse {
     private Meta meta;
 
-    private Map<String, Currency> data;
+    private Map<CurrencyCode, Currency> data;
 
     @lombok.Data
     public static class Meta {
@@ -21,7 +22,7 @@ public class CurrencyApiResponse {
     @lombok.Data
     public static class Currency {
         @JsonProperty("code")
-        private String code;
+        private CurrencyCode code;
 
         @JsonProperty("value")
         private BigDecimal value;
