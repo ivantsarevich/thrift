98b2f1cab205cfca7cca0388bb598768769588b4
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-28 02:21:10 +0300

    Update transaction feign client

--- thrift-limit/src/main/java/efr/iv/igr/thriftlimit/feign/TransactionFeignClient.java
+++ thrift-limit/src/main/java/efr/iv/igr/thriftlimit/feign/TransactionFeignClient.java
@@ -1,7 +1,11 @@
 package efr.iv.igr.thriftlimit.feign;
 
+import efr.iv.igr.thriftlimit.model.response.TransactionResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "transaction", url="${transaction.url}")
 public interface TransactionFeignClient {
+    @GetMapping
+    TransactionResponse getTransaction();
 }
