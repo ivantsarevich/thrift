c65a15549bf2ddb89551b64c2b9bf0379509c140
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-28 01:31:59 +0300

    Update CurrencyFeignClient

--- thrift-limit/src/main/java/efr/iv/igr/thriftlimit/feign/CurrencyFeignClient.java
+++ thrift-limit/src/main/java/efr/iv/igr/thriftlimit/feign/CurrencyFeignClient.java
@@ -1,7 +1,9 @@
 package efr.iv.igr.thriftlimit.feign;
 
+import efr.iv.igr.thriftlimit.model.response.CurrencyApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(name = "currency", url="${currencyapi.com}")
 public interface CurrencyFeignClient {
+    CurrencyApiResponse getCurrency();
 }
