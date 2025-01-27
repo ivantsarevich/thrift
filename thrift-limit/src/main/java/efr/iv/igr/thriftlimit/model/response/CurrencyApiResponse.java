77a08fe75881288e18ea993a7db3656aba94fde6
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-28 01:42:19 +0300

    Update currencyapi response model

--- thrift-limit/src/main/java/efr/iv/igr/thriftlimit/model/response/CurrencyApiResponse.java
+++ thrift-limit/src/main/java/efr/iv/igr/thriftlimit/model/response/CurrencyApiResponse.java
@@ -2,6 +2,7 @@ package efr.iv.igr.thriftlimit.model.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Map;
 
@@ -12,17 +13,17 @@ public class CurrencyApiResponse {
     private Map<String, Currency> data;
 
     @lombok.Data
-    public class Meta {
+    public static class Meta {
         @JsonProperty("last_updated_at")
         private Instant lastUpdatedAt;
     }
 
     @lombok.Data
-    public class Currency {
+    public static class Currency {
         @JsonProperty("code")
         private String code;
 
         @JsonProperty("value")
-        private double value;
+        private BigDecimal value;
     }
 }
