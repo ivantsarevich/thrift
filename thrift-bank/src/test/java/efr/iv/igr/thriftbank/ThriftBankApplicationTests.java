1dee71c0cfe50097df5459ccd144a191a80aaf27
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-27 20:05:05 +0300

    Disable tests

--- thrift-bank/src/test/java/efr/iv/igr/thriftbank/ThriftBankApplicationTests.java
+++ thrift-bank/src/test/java/efr/iv/igr/thriftbank/ThriftBankApplicationTests.java
@@ -1,8 +1,10 @@
 package efr.iv.igr.thriftbank;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class ThriftBankApplicationTests {
 
