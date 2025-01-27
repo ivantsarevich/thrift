5b18cb89dcf6b60a773b41f9cc8ef95fd6926d3b
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-27 20:15:47 +0300

    Add transaction service

---
+++ thrift-bank/src/main/java/efr/iv/igr/thriftbank/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,29 @@
+package efr.iv.igr.thriftbank.service.impl;
+
+import efr.iv.igr.thriftbank.model.entity.Transaction;
+import efr.iv.igr.thriftbank.repository.TransactionRepository;
+import efr.iv.igr.thriftbank.service.ITransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransactionServiceImpl implements ITransactionService {
+    private final TransactionRepository transactionRepository;
+
+    @Autowired
+    public TransactionServiceImpl(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    @Override
+    public Transaction createTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    @Override
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+}
