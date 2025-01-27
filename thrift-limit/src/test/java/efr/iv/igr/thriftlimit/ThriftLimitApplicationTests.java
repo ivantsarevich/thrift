7bfe247d65546ab40de01d327263eedc24ee72fd
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-27 23:49:44 +0300

    Disable tests

--- thrift-limit/src/test/java/efr/iv/igr/thriftlimit/ThriftLimitApplicationTests.java
+++ thrift-limit/src/test/java/efr/iv/igr/thriftlimit/ThriftLimitApplicationTests.java
@@ -1,8 +1,10 @@
 package efr.iv.igr.thriftlimit;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class ThriftLimitApplicationTests {
 
