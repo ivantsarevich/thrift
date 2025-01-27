1fd14f358f43f4dacaf8880c1fe67da9491d2235
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-27 20:28:10 +0300

    Change from injection class to injection interface

--- thrift-bank/src/main/java/efr/iv/igr/thriftbank/controller/TransactionController.java
+++ thrift-bank/src/main/java/efr/iv/igr/thriftbank/controller/TransactionController.java
@@ -4,7 +4,7 @@ import efr.iv.igr.thriftbank.mapper.TransactionMapper;
 import efr.iv.igr.thriftbank.model.entity.Transaction;
 import efr.iv.igr.thriftbank.model.request.TransactionRequest;
 import efr.iv.igr.thriftbank.model.response.TransactionResponse;
-import efr.iv.igr.thriftbank.service.impl.TransactionServiceImpl;
+import efr.iv.igr.thriftbank.service.ITransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +13,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/transactions")
 public class TransactionController {
-    private final TransactionServiceImpl transactionService;
+    private final ITransactionService transactionService;
 
     private final TransactionMapper transactionMapper;
 
     @Autowired
-    public TransactionController(TransactionServiceImpl transactionService, TransactionMapper transactionMapper) {
+    public TransactionController(ITransactionService transactionService, TransactionMapper transactionMapper) {
         this.transactionService = transactionService;
         this.transactionMapper = transactionMapper;
     }
