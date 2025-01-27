48b68d4c7258c6c9ae0f697fd267f7d1085bb9a8
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-28 01:48:44 +0300

    Add currency repository

---
+++ thrift-limit/src/main/java/efr/iv/igr/thriftlimit/repository/CurrencyRepository.java
@@ -0,0 +1,7 @@
+package efr.iv.igr.thriftlimit.repository;
+
+import efr.iv.igr.thriftlimit.model.entity.Currency;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyRepository extends JpaRepository<Currency, Long> {
+}
