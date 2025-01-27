d624700ec63d24caae18a4c57638c661fbcd65ee
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-27 20:45:20 +0300

    Initialize Thrift-Limit

---
+++ thrift-limit/src/main/java/efr/iv/igr/thriftlimit/ThriftLimitApplication.java
@@ -0,0 +1,13 @@
+package efr.iv.igr.thriftlimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThriftLimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThriftLimitApplication.class, args);
+    }
+
+}
---
+++ thrift-limit/src/test/java/efr/iv/igr/thriftlimit/ThriftLimitApplicationTests.java
@@ -0,0 +1,13 @@
+package efr.iv.igr.thriftlimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThriftLimitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
