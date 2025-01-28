010e4f16b9b0988717d04fa781e6c19e0c1afc75
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-28 14:13:08 +0300

    Update services

--- thrift-limit/src/main/java/efr/iv/igr/thriftlimit/service/ICurrencyService.java
+++ thrift-limit/src/main/java/efr/iv/igr/thriftlimit/service/ICurrencyService.java
@@ -1,9 +1,10 @@
 package efr.iv.igr.thriftlimit.service;
 
 import efr.iv.igr.thriftlimit.model.entity.Currency;
+import efr.iv.igr.thriftlimit.model.entity.CurrencyCode;
 
 public interface ICurrencyService {
     Currency createCurrency(Currency currency);
 
-    Currency getCurrency(String code);
+    Currency getCurrency(CurrencyCode code);
 }
--- thrift-limit/src/main/java/efr/iv/igr/thriftlimit/service/impl/CurrencyServiceImpl.java
+++ thrift-limit/src/main/java/efr/iv/igr/thriftlimit/service/impl/CurrencyServiceImpl.java
@@ -1,6 +1,7 @@
 package efr.iv.igr.thriftlimit.service.impl;
 
 import efr.iv.igr.thriftlimit.model.entity.Currency;
+import efr.iv.igr.thriftlimit.model.entity.CurrencyCode;
 import efr.iv.igr.thriftlimit.repository.CurrencyRepository;
 import efr.iv.igr.thriftlimit.service.ICurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +22,7 @@ public class CurrencyServiceImpl implements ICurrencyService {
     }
 
     @Override
-    public Currency getCurrency(String code) {
+    public Currency getCurrency(CurrencyCode code) {
         return currencyRepository.findFirstByCodeOrderByIdDesc(code);
     }
 }
