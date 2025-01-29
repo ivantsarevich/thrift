e55af136f9b08cc4c29dde14216291c8d1299f7b
Ivan E. <iamivanefremenkov@gmail.com>
2025-01-29 03:03:31 +0300

    Beautiful code

--- thrift-limit/src/main/java/efr/iv/igr/thriftlimit/feign/TransactionFeignClient.java
+++ thrift-limit/src/main/java/efr/iv/igr/thriftlimit/feign/TransactionFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "transaction", url="${transaction.url}")
+@FeignClient(name = "transaction", url = "${transaction.url}")
 public interface TransactionFeignClient {
     @GetMapping
     List<TransactionResponse> getTransaction();
