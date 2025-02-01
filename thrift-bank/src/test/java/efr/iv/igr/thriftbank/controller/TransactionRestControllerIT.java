49cc5bb1f7cdc599080f7fbddd892b95f3743bc7
Ivan E. <iamivanefremenkov@gmail.com>
2025-02-01 06:01:10 +0300

    Add integrate test for bank

--- thrift-bank/src/test/java/efr/iv/igr/thriftbank/controller/TransactionRestControllerIT.java
+++ thrift-bank/src/test/java/efr/iv/igr/thriftbank/controller/TransactionRestControllerIT.java
@@ -1,42 +1,80 @@
 package efr.iv.igr.thriftbank.controller;
 
-import efr.iv.igr.thriftbank.model.entity.Transaction;
-import efr.iv.igr.thriftbank.repository.TransactionRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.List;
-
+@Disabled
+@Sql("/db/migration/V2025_02_01__insert_test_data.sql")
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc(printOnlyOnFailure = false)
 class TransactionRestControllerIT {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private TransactionRepository transactionRepository;
+    @Test
+    void getTransactions_ReturnValidTransactionResponseEntity() throws Exception {
+        MockHttpServletRequestBuilder mockHttpServletRequestBuilder =
+                MockMvcRequestBuilders.get("/api/v1/transactions");
 
-//    @Test
-//    void getTransactions_ReturnValidTransactionResponseEntity() throws Exception {
-//        MockHttpServletRequestBuilder mockHttpServletRequestBuilder =
-//                MockMvcRequestBuilders.get("api/v1/transactions");
-//        transactionRepository.findAll()
-//                .addAll(
-//                        List.of(new Transaction())
-//                );
-//
-//        mockMvc.perform(mockHttpServletRequestBuilder)
-//                .andExpect(
-//                        MockMvcResultMatchers.status().isOk(),
-//                        MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
-//                        MockMvcResultMatchers.content().json("""[{}]""")
-//                );
-//    }
+        mockMvc.perform(mockHttpServletRequestBuilder)
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
+                        MockMvcResultMatchers.content().json("""
+                                                [
+                                                     {
+                                                         "id": 1,
+                                                         "accountFrom": 1,
+                                                         "accountTo": 1,
+                                                         "currency": "USD",
+                                                         "amount": 200,
+                                                         "category": "Product",
+                                                         "date": "2025-02-01T12:34:56.789Z"
+                                                     }
+                                                ]
+                                """));
+    }
+
+    @Test
+    void createTransaction_ReturnValidTransactionResponseEntity() throws Exception {
+        MockHttpServletRequestBuilder mockHttpServletRequestBuilder =
+                MockMvcRequestBuilders.post("/api/v1/transactions")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(("""
+                                        {
+                                              "accountFrom": 1,
+                                              "accountTo": 2,
+                                              "currency": "USD",
+                                              "amount": 200,
+                                              "category": "Product"
+                                        }
+                                """));
+
+        mockMvc.perform(mockHttpServletRequestBuilder)
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
+                        MockMvcResultMatchers.content().json("""
+                                        {
+                                              "accountFrom": 1,
+                                              "accountTo": 2,
+                                              "currency": "USD",
+                                              "amount": 200,
+                                              "category": "Product"
+                                        }
+                                """)
+                );
+    }
 }
